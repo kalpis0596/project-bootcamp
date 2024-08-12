ea5b691796bf3b4c03e41593ddea22c0ebb9226e
Yo Khafiz <khairulhafiz1988@gmail.com>
2024-08-12 20:09:03 +0800

    remove comment

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -10,6 +10,4 @@ terraform {
 provider "aws" {
   # Configuration options
   region = "ap-southeast-1"
-  /*access_key = "${var.AWS_ACCESS_KEY_ID}"
-  secret_key = "${var.AWS_SECRET_ACCESS_KEY}"*/
 }
