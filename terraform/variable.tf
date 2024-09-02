32e3b8028ff62f4f046a540adf54c3be555323de
Yo Khafiz <khairulhafiz1988@gmail.com>
2024-09-02 13:55:25 +0800

    test line code

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -8,9 +8,3 @@ variable "CLOUDFLARE_API_TOKEN" {
   type    = string
   default = "1EVwcEkygfv0ggAPWjyPW8iDSKBJvJMJSA1hzNG0"
 }
-
-# #Create cloudflare zone id
-# variable "cloudflare_zone_id" {
-#   type    = string
-#   default = "594e55635add266ae3c36fddbf5c185e"
-# }
